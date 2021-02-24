00c42eda3f7eedfddf0c3c8c5bccda2a09060b33
eduardosalaz <eduardosalaz@outlook.com>
2021-02-24 00:53:51 -0600

    primer commit, registrar un usuario funciona más o menos

---
+++ Assets/Scripts/ButtonHandlerLogin.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandlerLogin : MonoBehaviour
+{
+
+    public Button buttonInicio;
+    public Button buttonRegistro;
+    public GameObject inputField_texto;
+    public GameObject inputField_contraseña;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonInicio.onClick.AddListener(InicioGetInputOnClickHandler);
+        buttonRegistro.onClick.AddListener(RegistroGetInputOnClickHandler);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InicioGetInputOnClickHandler()
+    {
+        Debug.Log("logging");
+        string text = inputField_texto.GetComponent<TMP_InputField>().text;
+        Debug.Log(text);
+
+    }
+    public void RegistroGetInputOnClickHandler()
+    {
+        Debug.Log("al registro");
+        LoadGameLevel("Registro");
+
+
+    }
+    public void LoadGameLevel(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
---
+++ Assets/Scripts/ButtonHandlerRegistro.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandlerRegistro : MonoBehaviour
+{
+
+    public Button buttonRegistro;
+    public GameObject inputField_texto;
+    public GameObject inputField_contraseña;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonRegistro.onClick.AddListener(RegistrarseGetInputOnClickHandler);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RegistrarseGetInputOnClickHandler()
+    {
+        Debug.Log("logging");
+        string email = inputField_texto.GetComponent<TMP_InputField>().text;
+        string password = inputField_contraseña.GetComponent<TMP_InputField>().text;
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+
+
+
+
+    }
+}
